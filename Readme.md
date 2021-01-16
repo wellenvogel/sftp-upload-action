936647b46deb4195bc64587cc716f174bc0733ff
andreas <andreas@wellenvogel.de>
2021-01-16 13:47:59 +0100

    add build and Readme

---
+++ Readme.md
@@ -0,0 +1,45 @@
+# Sftp upload javascript action
+
+This actions uploads afile to an sftp server using [ssh2-sftp-client](https://www.npmjs.com/package/ssh2-sftp-client)
+
+## Inputs
+
+### `user`
+
+The sftp user name. If not set the action is skipped.
+
+### `server`
+
+The sftp server. If not set the action is skipped.
+
+### `privateKey`
+
+The private key. Use someting like `${{ secrets.REPO_KEY }}`.
+
+### `password`
+
+The password. One of private Key or password must be set.
+
+### `localFile`
+
+**Required** The local file to be uploaded.
+
+### `remoteDir`
+
+**Required** The remote directory. Will be created if not existing.
+
+
+The time we greeted you.
+
+## Example usage
+
+```
+uses: wellenvogel/action-upload-sftp@v1.0
+with:
+  user: ${{ secrets.REPO_USER }}
+  server: ${{ secrets.REPO_SERVER }}
+  privateKey: ${{ secrets.REPO_KEY }}
+  localFile: 'archive.tgz'
+  remoteDir: '/uploads/example'
+
+```  
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 const core=require('@actions/core');
 const Client=require('ssh2-sftp-client');
-async function upload(host,user,key,local,remoteDir){
+async function upload(host,user,key,pass,local,remoteDir){
     let remoteFile=local.replace(/.*\//,'');
     let sftp=new Client();
     var result= await sftp.connect({
         host:host,
         user:user,
-        privateKey:key
+        privateKey:key,
+        password:pass
     }).then(()=>{
         return sftp.exists(remoteDir);
     })
@@ -24,31 +25,35 @@ async function upload(host,user,key,local,remoteDir){
     })
     .then(()=>sftp.end())
     .then(()=>'')
-    .catch((error)=>{sftp.end();return error});
+    .catch((error)=>{sftp.end().catch(()=>'');return error});
     console.log("upload returns: "+result);
     return result;
 }
 
-try{
-    const user=core.getInput('user')
-    const server=core.getInput('server')
-    const key=core.getInput('privateKey')
-    const pass=core.getInput('password')
-    if (! user || ! server){
-        console.log("user or server not set, skip upload");
-    }
-    else{
-        if (!key && ! pass){
-            throw new Error("either privateKey or password must be set");
+async function main(){
+    try{
+        const user=core.getInput('user')
+        const server=core.getInput('server')
+        const key=core.getInput('privateKey')
+        const pass=core.getInput('password')
+        if (! user || ! server){
+            console.log("user or server not set, skip upload");
         }
-        let result=await upload(server,user,key,
-            core.getInput('localFile'),
-            core.getInput('remoteDir'));
-        if (result !== ''){
-            throw new Error(result);
-        }
-    }
+        else{
+            if (!key && ! pass){
+                throw new Error("either privateKey or password must be set");
+            }
+            let result=await upload(server,user,key,pass,
+                core.getInput('localFile'),
+                core.getInput('remoteDir'));
+            if (result !== ''){
+                throw new Error(result);
+            }
+        }   
 
-}catch(e){
-    core.setFailed(e.message)
+    }catch(e){
+        core.setFailed(e.message)
+    }
 }
+
+main();
